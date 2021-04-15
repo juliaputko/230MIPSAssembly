101865c5a9fb2b7d68bd5e37954ad30bba8335fa
Julia Putko <81587103+juliaputko@users.noreply.github.com>
2021-04-15 11:51:59 -0700

    Update a3-part-A.asm

--- a3/a3-part-A.asm
+++ a3/a3-part-A.asm
@@ -1,71 +1,81 @@
-# UVic CSC 230, Fall 2020
-# Assignment #1, part C
-
-# Student name: Keanu Reeves
-# Student number: V1234576
-
-
-# Compute M / N, where M must be in $8, N must be in $9,
-# and M / N must be in $15.
-# N will never be 0
-
-
-.text
-start:
-
-	# $8 - first testcase
-	# $9 - second testcase
-	# $11 - stores 1 or 0 after < comparison
-	# $15 - result after divison
-	lw $8, testcase2_M
-	lw $9, testcase2_N
-
+#Julia Putko
+#V00889506
+	.data
+ARRAY_A:
+	.word	21, 210, 49, 4
+ARRAY_B:
+	.word	21, -314159, 0x1000, 0x7fffffff, 3, 1, 4, 1, 5, 9, 2
+ARRAY_Z:
+	.space	28
+NEWLINE:
+	.asciiz "\n"
+SPACE:
+	.asciiz " "
+		
+	
+	.text  
+main:	
+	la $a0, ARRAY_A
+	addi $a1, $zero, 4
+	jal dump_array
+	#output for this would be 
+	#21, 210, 49, 4
+	
+	la $a0, ARRAY_B
+	addi $a1, $zero, 11
+	jal dump_array
+	
+	la $a0, ARRAY_Z
+	lw $t0, 0($a0)   
+	addi $t0, $t0, 1    
+	sw $t0, 0($a0)    
+	addi $a1, $zero, 9  
+	jal dump_array
+		
+	addi $v0, $zero, 10
+	syscall
 
 # STUDENTS MAY MODIFY CODE BELOW
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-	addi $15, $15, 0  	# set $15 to 0
-	li $11, 0		# set $11 to 0
-
-	slt $11, $8, $9  	#check if 8<9, then put 1 in R11, 0 otherwise
-	beq $11, 1, exit
 	
-loop: 
-	sub $10, $8, $9  	#m-n stored in R10
-	add $8, $10, 0 		#quotient is now the dividend
-	addi $15, $15, 1 	#increment R15 by 1 everything we do a division 
-	slt $11, $8, $9  	#check if 8<9, then put 1 in R11, 0 otherwise
-	beq $11, 0, loop 	#branch to loop if 0 is in $11 bc that means 8 is not < 9 and subtraction wont be negative
 	
-	#nop
-	#addi $15, $0, -10
 
+dump_array:
+
+#write this procedure 
+#	$a0: holds address of first integer in an array of integers
+#	$a1: holds the number of integers to be output on the consle in a single line 
+#	return value: none
+#	
+#	system call 1: is used to print an integer on teh console; the integer to be printed is sotred in $a0 while the value 1 is 
+#	stored in $v0
+#	system call 4: is ued to print a null terminated string on the console; the address of the string to be printed is storedin $a0
+#	while value 4 is stored in $v0
+
+	move $t2, $a0
+loop:
+
+	lbu $a0, 0($t2)
+	addi $v0, $zero, 1
+	syscall
+	
+	la $a0, SPACE
+	addi $v0, $zero, 4
+	syscall
+	
+	addi $t2, $t2, 4
+	lbu $a0, 0($t2)
+	addi $a1, $a1, -1 
+	bne $a1, $zero, loop
+	
+	la $a0, NEWLINE
+	addi $v0, $zero, 4
+	syscall
+	#jr $ra
+	
+
+	
+	
+	
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # STUDENTS MAY MODIFY CODE ABOVE
-
-exit:
-	add $2, $0, 10
-	syscall
-		
-
-.data
-
-# testcase1: 370 / 120 = 3
-#
-testcase1_M:
-	.word	370
-testcase1_N:
-	.word 	120
-	
-# testcase2: 24156 / 77 = 313
-#
-testcase2_M:
-	.word	24156
-testcase2_N:
-	.word 	77
-	
-# testcase3: 33 / 120 = 0
-#
-testcase3_M:
-	.word	33
-testcase3_N:
-	.word 	120
